83060d857b001cf17174eb29f9b8916a1175f280
Scott.Hazelhurst <scott.hazelhurst@wits.ac.za>
2021-05-03 18:26:39 +0200

    Monday work

--- hapsimul/hapsimul.c
+++ hapsimul/hapsimul.c
@@ -10,30 +10,41 @@ typedef  unsigned char u_char;
 
 #include <sys/types.h>
 #include <stdlib.h>
+#include "hash.h"
 
 #define max_cluster 3
 
 
 
-static char * allowed_options="e:n:h:";
+static char * allowed_options="e:n:H:p:";
 
 static struct option long_options[] = {
   {"error-rate",required_argument,0,'e'},
   {"num-gen",required_argument,0,'n'},
   {"haplo-len",required_argument,0,'H'},
+  {"pca",required_argument,0,'p'},  
   {"gen-pheno",required_argument,0,'g'},  
   {0,0,0,0}
 };
 
 /* Global variables -- set by default and perhaps program options and never modified */
 
+static const int num_sig_pcs=1;
+
 static double opt_err_rate=0.001;     // New errors introduced
 static int   opt_num_gen=1000;    // Number of individuals generated
 static int   opt_hap_len  = 50;       // Size of haplo window
 static char   *open_gen_pheno = 0;
-
+static char *opt_pca_file = (char *) 0;
+static int   opt_pca_num_top;
 static char * input_fname, * output_fname;
 
+static char **bim_table;  // names of SNPs
+
+
+/* PCs -- needs to be global for functional parameter */
+
+float **pc = (float **) 0;
 
 
 void process_options(int argc, char * argv[]) {
@@ -48,8 +59,16 @@ void process_options(int argc, char * argv[]) {
     case 'n':
       opt_num_gen=atoi(optarg);
       break;
-    case 'h' :
+    case 'H' :
       opt_hap_len=atoi(optarg);
+      break;
+    case 'g' :
+      opt_hap_len=atoi(optarg);
+      break;
+    case 'p' :
+      opt_pca_file=optarg;
+      sscanf(argv[optind+1],"%d",&opt_pca_num_top);
+      optind++;
       break;
     }
   }
@@ -68,7 +87,7 @@ void process_options(int argc, char * argv[]) {
 }
 
 static int  num_s, num_v, num_clusters, bsize, new_bsize;
-static unsigned char * old_bed;
+static unsigned char * old_bed, * new_bed;
 
 // for extracting out the bits in a byte (it's in binary to help visualisation)
 static int mask[4] = {0b000000011,0b00001100,0b00110000, 0b11000000};
@@ -82,13 +101,25 @@ int getBim(char * bim, int *chrom_start) {
   FILE *f;
   float cm;
   int chrom, pos, num_read; // data in bim file
-  int next_chrom, line_num;
-  char rs[512], maj[32100], min[32100];
+  int next_chrom, line_num,m;
+  char rs[1024000], maj[32100], min[32100];
 
-  
+
+  // first find how many lines there
   f = fopen(bim,"r");
-  num_read = fscanf(f,"%d%s%f%d%s%s\n",&chrom, rs, &cm, &pos, maj, min);
   next_chrom = line_num = 0;
+  while (fscanf(f,"%30000[^\n]\n",rs)>0) {
+    if (strlen(rs)>31000) {
+      printf("There a line in the BIM file that is too long <%s>\n",rs);
+      exit(-16);
+    }
+    line_num++;
+  }
+  fclose(f);
+  f = fopen(bim,"r");
+  bim_table = (char  **) calloc(line_num,sizeof(char *));
+  line_num=0;
+  num_read = fscanf(f,"%d%s%f%d%s%s\n",&chrom, rs, &cm, &pos, maj, min);
   while (num_read == 6) {
       while(next_chrom<chrom) { // missing chroms in bim file
 	  chrom_start[next_chrom]=-1;
@@ -98,27 +129,19 @@ int getBim(char * bim, int *chrom_start) {
 	 chrom_start[next_chrom] = line_num;
 	 next_chrom++;
      }
-      line_num++;
-      num_read = fscanf(f,"%d%511s%f%d%32000s%32000s\n",
+     num_read = fscanf(f,"%d%511s%f%d%32000s%32000s\n",
 			&chrom, rs, &cm, &pos, maj, min);
-      if ((strlen(min)>=31998) || (strlen(maj)>=31998)){
-	printf("There is a allele of greater than length 32000 in the bim file (line %d)\n",
-	       line_num);
-	exit(-13);
-      }
-      if ((strlen(rs)>510)) {
-	printf("There is a SNP ID  of greater than length 500 in the bim file");
+     if ((strlen(rs)>510)) {
+	printf("There is a SNP ID  of greater than length 500 in the bim file"
+	       "<%s>",rs);
 	exit(-14);
-      }
+     }
+     hash_add(rs,line_num);
+     bim_table[line_num]=(char *) malloc(strlen(rs)+1);
+     strcpy(bim_table[line_num],rs);
+     line_num++;
+
   }
-    while (next_chrom<27) {
-      chrom_start[next_chrom]=-1;
-      next_chrom ++;
-  }
-  /*
-  for(next_chrom = 0; next_chrom<27; next_chrom++) {
-    printf("chrom[%d] = %d\n",next_chrom, chrom_start[next_chrom]);
-    }*/
   fclose(f);
   return line_num;
 }
@@ -278,20 +301,155 @@ char  * mutateData(char * mute_file, unsigned char * new_bed, int num_new) {
 }
 
 
+
+
+void getPCs(int num_v, float **pc, int * pc_index, int *num_v_pc) {
+  FILE *f;
+  int chrom, p, m, snp;
+  char snp_name[4096], all1[4096], all2[4096];
+
+  for(p=0; p<num_sig_pcs;p++)
+    pc[p]=(float *) calloc(num_v,sizeof(float));
+  f=fopen(opt_pca_file,"r");
+  if(f<=0) {
+    printf("Failed to open file <%s>\n",opt_pca_file);
+    exit(-20);
+  }
+
+  for(snp=0; snp<num_v;snp++) {
+    m=fscanf(f,"%d%4092s%4092s%4092s ",&chrom,snp_name,all1,all2);
+    if (m<0) break;
+    if ((strlen(snp_name)>4090)||(strlen(all1)>4090)||(strlen(all2)>4090)) {
+      printf("Illegal SNP or allele at %s\n",snp_name);
+      exit(-15);
+    }
+    for(p=0; p<num_sig_pcs;p++)  {
+      m=fscanf(f,"%f",&pc[p][snp]);
+      if (m<0) printf("%d %d %d\n",snp,num_v,m);
+    }
+    m = hash_find(bim_table,snp_name);
+    pc_index[snp]=m;  // The entry "snp" in the PC table is "m" in the bim_table
+    fscanf(f,"%[^\n]",all1);
+  }
+  *num_v_pc=snp;
+  fclose(f);
+}
+
+static int pcomp( void *data, const void * id, const void * jd) {
+  float *pc;
+  int i, j;
+  pc=(float *) data;
+  i = *(int *) id;
+  j =* (int *) jd;
+  if (((i==133153)||(j==133153))&&((i==138895)||(j==138895)))
+    printf("i=%d (%f), j=%d (%f)\n",i,pc[i],j,pc[j]);
+  return pc[i]-pc[j];
+}
+
+void swap(int * index, int i, int j) {
+  int  tmp;
+  tmp=index[i];
+  index[i]=index[j];
+  index[j]=tmp;
+}
+
+void quick_sort(int *index, float * pc, int start, int finish) {
+  int mid=0;
+  float pivot;
+  
+  mid=(start+finish)/2;
+  pivot=pc[index[mid]];
+  swap(index,mid,start);
+  mid=start;
+  for(int i=start; i<finish; i++) {
+    if (pc[index[i]]<pivot) {
+      mid++;
+      swap(index,mid,i);
+    }
+  }
+  swap(index,start,mid);
+  if (start<mid)  quick_sort(index,pc,start,mid);
+  if (mid+1<finish) quick_sort(index,pc,mid+1,finish);
+}
+  
+
+int max(int a, int b) {
+  return a > b  ? a :b;
+}
+
+int min(int a, int b) {
+  return a < b  ? a :b;
+}
+
+#define ancestry_block 2
+
+void copy_byte(int dst, int src, int v) {
+  int start, shift;
+  unsigned char *dptr, *sptr, src_byte, dst_byte;
+
+  shift = ((dst&0x03)-(src&0x03))<<1;   
+  start = max(0,v-ancestry_block);
+  dptr = new_bed+(long) 3+((long) start)*((long) new_bsize)+(dst/4);
+  sptr= old_bed+((long) start)*((long) bsize)+src/4;
+  for(int i=start; i<min(v+ancestry_block,num_v); i++) {
+    src_byte=(*sptr)&mask[src&0x03];
+    dst_byte=(*dptr)&(~mask[dst&0x04]);
+    if (shift>0)
+     *dptr = dst_byte |  (src_byte << shift);
+    else
+     *dptr = dst_byte | (src_byte >> -shift);
+    sptr++;
+    dptr++;
+  }
+}
+  
+
+void copy_from_source(int dst, int src, int * index, int num_v_pc) {
+  for (int i=0; i<opt_pca_num_top/8; i++) {
+    copy_byte(dst,src,index[i]);
+    copy_byte(dst,src,index[num_v_pc-i]);    
+  }
+}
+
+void managePCs(int num_v) {
+  float **pc;
+  int index[num_v], pc_index[num_v], v;  // NB: pc_index bigger than it needs to be
+  int num_v_pc; // Number variants in the PC file
+
+  // We have two indexes here -- "index" is an index into the SNPs that
+  // are in the PC. We sort them in order to find the ones with most weight
+  // then pc_index tells us for each SNP in the PCA where it is in the 
+  pc = (float **) calloc(num_sig_pcs,sizeof(float *));
+  getPCs(num_v, pc ,pc_index, &num_v_pc);
+  // now get the  most significant SNPs in the PC fle
+  for(int p=0; p<num_sig_pcs; p++) {
+    for(v=0;v<num_v;v++) index[v]=v;
+    quick_sort(index,pc[p],0,num_v_pc);
+    // find where in the BIM file these SNPs are
+    for(int i=0; i<num_v_pc; i++) index[i]=pc_index[index[i]];
+    // copy
+    for(int dst=0; dst<opt_num_gen; dst++) {
+      int src=arc4random_uniform(num_s); // who we will copy
+      copy_from_source(dst,src,index,num_v_pc); 
+    }
+  }
+}
+
+
+
 int main(int argc, char *argv[]) {
 
   int  *clusters;
   char *is_case;
-  unsigned char * new_bed;
+
   char  bed[4096], bim[4096], fam[4096];
 
   process_options(argc, argv);
-
+  hash_init();
   // max fname is 4096 including suffix!
   snprintf(bed,4096,"%s.bed%c",input_fname,0);
   snprintf(bim,4096,"%s.bim%c",input_fname,0);
   snprintf(fam,4096,"%s.fam%c",input_fname,0);  
-
   new_bsize        = (opt_num_gen+3)/4;
   num_s             = getFam(fam);
   bsize               = (num_s+3)/4;
@@ -299,11 +457,14 @@ int main(int argc, char *argv[]) {
   old_bed           = readBed(bed);
   new_bed         = allocBed(opt_num_gen);
 
+
   generateSamples(new_bed);
 
   is_case = malloc(opt_num_gen);
   bzero(is_case,opt_num_gen);
     //mutateData(argv[4],new_bed, num_new);
+
+  if (opt_pca_file) managePCs(num_v);
   
   outputData(output_fname, new_bed, is_case);
  
